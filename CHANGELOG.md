5e8f74c50e2430f63d54dd1da587ca650b26b7c6
Ahmad Einieh <66053897+ahmad-einieh@users.noreply.github.com>
2023-09-26 09:08:13 +0300

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+### [1.1.3] - 25/04/2022
+
+* added background color on text field
+* added input formatter
+
+### [1.1.2] - 28/01/2021
+
+* Added OtpTextField Controller
+
+## [1.1.1] - 17/05/2021
+
+* Added option to set background/border color using OtpFieldStyle
+
+## [1.1.0] - 06/03/2021
+
+* Added Null-Safety
+* Added Quick paste functionality
+
+## [1.0.2] - 11/01/2021
+
+* Fixed issue with field style underline not working
+
+## [1.0.1] - 24/03/2020
+
+* Added option to set keyboard Type
+
+
+## [1.0.0] - 18/03/2020
+
+* First release of the package
---
+++ example/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:otp_text_field/otp_text_field.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Awesome Credit Card Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Awesome Credit Card Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  OtpFieldControllerV2 otpController = OtpFieldControllerV2();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.cleaning_services),
+        onPressed: () {
+          print("Floating button was pressed.");
+          otpController.clear();
+          // otpController.set(['2', '3', '5', '5', '7']);
+          // otpController.setValue('3', 0);
+          // otpController.setFocus(1);
+        },
+      ),
+      body: Center(
+        child: OTPTextFieldV2(
+            controller: otpController,
+            length: 5,
+            width: MediaQuery.of(context).size.width,
+            textFieldAlignment: MainAxisAlignment.spaceAround,
+            fieldWidth: 45,
+            fieldStyle: FieldStyle.box,
+            outlineBorderRadius: 15,
+            style: TextStyle(fontSize: 17),
+            onChanged: (pin) {
+              print("Changed: " + pin);
+            },
+            onCompleted: (pin) {
+              print("Completed: " + pin);
+            }),
+      ),
+    );
+  }
+}
